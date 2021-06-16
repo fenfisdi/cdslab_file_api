e256c963f4026f36766b76176958c9ae030bae75
miguelgomezg <miguelgomezgarcia@yahoo.com>
2021-06-16 10:56:48 -0500

    fix

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -8,7 +8,6 @@ from src.utils.patterns import Singleton
 class MongoEngine(metaclass=Singleton):
     def __init__(self):
         self.mongo_uri = environ.get('MONGO_URI')
-        self.mongo_uri = "mongodb://cdsuser:cdspass@192.168.20.42:27017/cdslib_folder?authSource=admin"
 
     def get_connection(self):
         return connect(host=self.mongo_uri)
