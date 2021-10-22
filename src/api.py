561b64a5bcdcf341a4905e9ec4b625dc9e69dee7
David Peña <dpenah@intergrupo.com>
2021-10-22 11:46:37 -0500

    Feature: Implement Error Service

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ from os import environ
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.responses import PlainTextResponse
 
 from src.config import fastApiConfig
 from src.db import MongoEngine
@@ -12,9 +13,16 @@ from src.routes import (
     root_routes,
     scrapping_routes
 )
+from src.services import ErrorAPI
 
 db = MongoEngine().get_connection()
 app = FastAPI(**fastApiConfig)
+
+
+@app.exception_handler(Exception)
+def http_error_report(_, exc: Exception):
+    ErrorAPI.report_error(str(exc), code=500)
+    return PlainTextResponse("Internal Server Error", status_code=500)
 
 
 app.add_middleware(
